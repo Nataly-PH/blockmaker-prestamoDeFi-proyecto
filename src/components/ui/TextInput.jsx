3a1fafc28d398768f0c903e9182f3f8a12f49712
nataly <15marzonataly@gmail.com>
2024-03-26 21:45:19 +0100

    Añadir componente TextInput

---
+++ src/components/ui/TextInput.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types"
+
+export default function TextInput({ type = "text", onChange, value, placeholder, disabled }) {
+    return (
+        <input 
+            type={type} 
+            onChange={onChange} 
+            value={value} 
+            placeholder={placeholder} 
+            disabled={disabled}     
+            className ="border border-gray-300 rounded-lg p-2 outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition durantion-300 ease-in-out">
+
+        </input>
+
+
+    )
+    
+}
+
+TextInput.propTypes = {
+    type: PropTypes.string,
+    onChange: PropTypes.func,
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+    disabled: PropTypes.bool
+
+}
