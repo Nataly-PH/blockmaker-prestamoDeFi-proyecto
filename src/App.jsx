d57354600f73c325d3cef2324651aaf0f7b195a0
nataly <15marzonataly@gmail.com>
2024-03-28 19:36:05 +0100

    Actualizar App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { ConnectKitProvider } from "connectkit";
 import { WagmiConfig } from "wagmi";
 import { AppLayout } from "./components/ui/layauts";
 import { config } from "./config/wagmi";
-import { Home } from "./pages";
+import { InicioYAutenticacion, GestionUsuario, OperacionesDePrestamos } from "./pages";
+import { Home } from "./pages/Pagina_1";
+import { Routes, Route } from "react-router-dom";
 
 
 
@@ -12,7 +14,14 @@ function App() {
     <WagmiConfig config={config}>
       <ConnectKitProvider>
         <AppLayout>
+          <Routes>
+            <Route path="/" element={<InicioYAutenticacion></InicioYAutenticacion>}></Route>    
+            <Route path="/usuarios" element={<GestionUsuario></GestionUsuario>}></Route>
+            <Route path="/prestamos" element={<OperacionesDePrestamos></OperacionesDePrestamos>}></Route>
+          </Routes>
+
           <Home></Home>
+      
         </AppLayout>
       </ConnectKitProvider>
     </WagmiConfig>
