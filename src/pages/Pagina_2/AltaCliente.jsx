5e2b9bb2af8696fcccad866cd11858c5642b4ed7
nataly <15marzonataly@gmail.com>
2024-03-28 19:35:39 +0100

    Añadir Alta cliente y alta prestamista

---
+++ src/pages/Pagina_2/AltaCliente.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function AltaCliente() {
+  // Implementa la lógica del componente AltaCliente aquí
+  return (
+    <div>
+      {/* Contenido del componente AltaCliente */}
+      Alta de Cliente
+    </div>
+  );
+}
+
+export default AltaCliente;
---
+++ src/pages/Pagina_2/AltaPrestamista.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function AltaPrestamista() {
+  // Implementa la lógica del componente AltaPrestamista aquí
+  return (
+    <div>
+      {/* Contenido del componente AltaPrestamista */}
+      Alta de Prestamista
+    </div>
+  );
+}
+
+export default AltaPrestamista;
