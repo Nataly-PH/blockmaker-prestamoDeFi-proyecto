d337cc37770d37fa8efb61342bb1ded49f8a497d
nataly <15marzonataly@gmail.com>
2024-03-28 19:39:54 +0100

    Añadir indice pagina 2

---
+++ src/pages/Pagina_2/index.js
@@ -0,0 +1,3 @@
+export { default as AltaCliente } from '../Pagina_2/AltaCliente';
+export { default as AltaPrestamista } from '../Pagina_2/AltaPrestamista';
+export { default as GestionUsuario } from '../Pagina_3/OperacionesDePrestamos';
