c4d0614f081f6279a88bf2202b59dcddc2824988
nataly <15marzonataly@gmail.com>
2024-03-28 19:34:53 +0100

    Añadir InicioYAutenticacion

---
+++ src/pages/Pagina_1/InicioYAutenticacion.jsx
@@ -0,0 +1,12 @@
+import  Home   from '../Pagina_1/Home';
+import { Navbar } from '../../components';
+
+export default function InicioYAutenticacion() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Página 1</h1>
+            <Home></Home>
+            <Navbar></Navbar>
+        </>
+    )
+}
